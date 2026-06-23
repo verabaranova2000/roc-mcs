f0241fdc2d82cdfa48f891900e336bc2d55d2566
verabaranova2000 <126353652+verabaranova2000@users.noreply.github.com>
2026-06-23 12:22:12 +0000

    Добавлен config.py

---
+++ src/roc_mcs/fitting/trajectory/config.py
@@ -0,0 +1,64 @@
+MODEL_TRAJECTORY = {
+    "lorentz": {
+        "param_keys": ("S", "theta0", "gamma"),
+        "rel_spans": (0.10, 0.03, 0.20),
+        "ns": (20, 10, 20),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "gauss": {
+        "param_keys": ("S", "theta0", "sigma"),
+        "rel_spans": (0.10, 0.03, 0.20),
+        "ns": (25, 11, 25),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "gauss_us": {
+        "param_keys": ("S", "theta0", "sigma", "Delta"),
+        "rel_spans": (0.10, 0.05, 0.20, 0.20),
+        "ns": (20, 10, 20, 20),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "voigt": {
+        "param_keys": ("S", "theta0", "sigma", "gamma"),
+        "rel_spans": (0.10, 0.03, 0.20, 0.20),
+        "ns": (25, 11, 25, 25),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "pvoigt": {
+        "param_keys": ("S", "theta0", "H", "eta"),
+        "rel_spans": (0.10, 0.03, 0.20, 0.15),
+        "ns": (25, 11, 25, 21),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "emg": {
+        "param_keys": ("S", "theta0", "sigma", "gamma", "lam"),
+        "rel_spans": (0.10, 0.03, 0.20, 0.20, 0.25),
+        "ns": (21, 9, 21, 21, 21),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+
+    "split_voigt": {
+        "param_keys": ("S", "theta0", "beta_Gl", "beta_Cl", "beta_Gr", "beta_Cr"),
+        "rel_spans": (0.10, 0.03, 0.20, 0.20, 0.20, 0.20),
+        "ns": (9, 9, 9, 9, 9, 9),
+        "rel_keep": 1.05,
+        "max_keep": None,
+        "q_alpha": 0.01,
+    },
+}
