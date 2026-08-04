db3bab94226532bd33e8a1fd80065f00130f85a7
Vera Baranova <verabaranova2000@mail.ru>
2026-08-04 15:05:17 +0300

    Добавлена функция export_motor_scans

--- src/roc_mcs/io/sardana_h5.py
+++ src/roc_mcs/io/sardana_h5.py
@@ -32,3 +32,71 @@ def load_motor_scan(file, entry_id):
             "theta": g["th"][:],
             "intensity": g["ct0"][:],
         }
+    
+
+
+    from pathlib import Path
+import numpy as np
+import h5py
+
+
+def export_motor_scans(
+    entry_ids,
+    h5_file,
+    out_folder,
+):
+    """
+    Экспортирует моторные сканы из h5 в отдельные txt-файлы.
+
+    Parameters
+    ----------
+    entry_ids : int | sequence[int]
+        Номер(а) entry.
+    h5_file : str | Path
+        Путь к TPC_data.h5.
+    out_folder : str | Path
+        Папка для сохранения файлов.
+    """
+    if np.isscalar(entry_ids):
+        entry_ids = [entry_ids]
+
+    out_folder = Path(out_folder)
+    out_folder.mkdir(parents=True, exist_ok=True)
+
+    with h5py.File(h5_file, "r") as f:
+        for entry_id in entry_ids:
+            g = f[f"entry{entry_id}"]["measurement"]
+
+            theta = g["th"][:]
+            intensity = g["ct0"][:]
+            data = np.column_stack([theta, intensity])
+            outfile = out_folder / f"scan{entry_id}.txt"
+            np.savetxt(
+                outfile,
+                data,
+                header="theta\tintensity",
+                fmt="%.8f",
+                delimiter="\t",
+            )
+            print(outfile)
+
+
+# Использование
+# export_motor_scans(
+#     entry_ids=[320, 321, 322],
+#     h5_file=H5_FILE,
+#     out_folder="motor_scans",
+# )
+
+# или
+# export_motor_scans(
+#     entry_ids=321,
+#     h5_file=H5_FILE,
+#     out_folder="motor_scans",
+# )
+
+# Получится
+# motor_scans/
+#     scan320.txt
+#     scan321.txt
+#     scan322.txt
